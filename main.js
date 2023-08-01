27d6cb14d44fc0fbfec6193d878d7652c5ed91d7
Surya Gempita <95684526+CaptKeroro@users.noreply.github.com>
2023-08-01 14:38:02 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 let prevScrollPos = window.pageYOffset;
+let isHeaderVisible = true;
+const headerHeight = document.querySelector("header").offsetHeight;
 
 function toggleHeaderVisibility() {
   const currentScrollPos = window.pageYOffset;
-  const header = document.querySelector("header");
-  const headerHeight = header.offsetHeight;
 
   if (prevScrollPos > currentScrollPos) {
     // Scroll up
-    header.style.top = "0";
+    document.querySelector("header").style.top = "0";
   } else {
     // Scroll down
-    header.style.top = `-${headerHeight}px`;
+    document.querySelector("header").style.top = `-${headerHeight}px`;
   }
 
   prevScrollPos = currentScrollPos;
@@ -19,10 +19,43 @@ function toggleHeaderVisibility() {
 // Event listener to toggle header visibility on scroll
 window.addEventListener("scroll", toggleHeaderVisibility);
 
-// Function to show the header
-function showHeader() {
-  document.querySelector("header").style.top = "0";
+// Function to show or hide the header
+function toggleHeader() {
+  if (isHeaderVisible) {
+    // Header is visible, hide it
+    document.querySelector("header").style.top = `-${headerHeight}px`;
+  } else {
+    // Header is hidden, show it
+    document.querySelector("header").style.top = "0";
+  }
+
+  isHeaderVisible = !isHeaderVisible;
 }
 
-// Event listener to show the header on click
-document.querySelector("body").addEventListener("click", showHeader);
+// Event listener to show or hide the header on click anywhere on the page
+document.addEventListener("click", function (event) {
+  const clickedElement = event.target;
+  const header = document.querySelector("header");
+
+  if (!header.contains(clickedElement)) {
+    // Clicked element is not inside the header, toggle header visibility
+    toggleHeader();
+  }
+});
+
+// Internal navigation - smooth scroll to target with compensating for header height
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+    const headerOffset = headerHeight + 32; // Adjust this value according to your header height
+    const elementPosition = target.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - headerOffset;
+
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: "smooth"
+    });
+  });
+});
